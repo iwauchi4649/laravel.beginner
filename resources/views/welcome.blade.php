8993969f865c19f40cd9720f04030863ad39c341
Syoma Iwauchi <syoma4645@gmail.com>
2020-05-10 15:11:50 +0900

    logo create

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>麻雀成績記録</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -32,6 +32,17 @@
 
             .position-ref {
                 position: relative;
+            }
+
+            .top-left {
+                position: absolute;
+                left: 10px;
+                top: 10px;
+            }
+
+            .logo{
+                max-width: 250px;
+                max-height: 50px;
             }
 
             .top-right {
@@ -65,19 +76,26 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+            <div class="header-bar">
+                <div class="top-left links">
+                    <a href="{{ url('/home') }}">
+                        <img src="logo-row.svg" alt="logo" class="logo">
+                    </a>
                 </div>
-            @endif
+                @if (Route::has('login'))
+                    <div class="top-right links">
+                        @auth
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ route('login') }}">Login</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+            </div>
 
             <div class="content">
                 <div class="title m-b-md">
